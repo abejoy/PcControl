5638210d1fc4e84fb71ed716045a6a017ef9fb39
abrah <abrahamjoys98@gmail.com>
2020-08-27 17:54:15 +0100

    added computer side code

---
+++ Sleep.bat
@@ -0,0 +1,3 @@
+rem script before sleep
+curl /WAIT www.abrahamjoys.com/set-sleep-status/True
+Rundll32.exe Powrprof.dll,SetSuspendState Sleep
---
+++ run_on_pc.py
@@ -0,0 +1,41 @@
+import os
+import time
+import vlc
+import threading
+import subprocess
+from flask import Flask, json
+
+api = Flask(__name__)
+my_song = None
+
+@api.route('/playParot', methods=['GET'])
+def playParot():
+    global my_song
+    my_song = vlc.MediaPlayer("Parot.mp3")
+    my_song.play()
+    # os.startfile('Parot.mp3')
+    return "playing"
+
+
+@api.route('/stop-parot', methods=['GET'])
+def stopParot():
+    global my_song
+    if(my_song != None):
+        my_song.stop()
+    return 'stoped'
+
+
+@api.route('/sleep', methods=['GET'])
+def sleepPc():
+    def do_work():
+        stopParot()
+        subprocess.call(['Sleep.bat'])
+        time.sleep(1)
+
+    thread = threading.Thread(target=do_work)
+    thread.start()
+
+    return 'sleeping'
+
+
+api.run(host='0.0.0.0', port=80)
