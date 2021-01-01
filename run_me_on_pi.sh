77dae8a4b585f8e3e235e8207cb4d8d89778cc78
Abe <abrahamjoys98@gmail.com>
2021-01-01 18:33:01 +0000

    commited packagelock cganges

--- run_me_on_pi.sh
+++ run_me_on_pi.sh
@@ -1,4 +1,4 @@
 cd pc-app
-gnome-terminal -e "sudo serve -s build -l 80"
+xterm -e "sudo serve -s build -l 80"
 cd ../pc-backend
-gnome-terminal -e "sudo python3 run_on_pi.py"
+xterm -e "sudo python3 run_on_pi.py"
