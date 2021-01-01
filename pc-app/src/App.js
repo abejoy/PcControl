abba6aab37efc74df4aa080f4152329f3e0b57b1
abrah <abrahamjoys98@gmail.com>
2021-01-01 17:46:27 +0000

    lower cased components

--- pc-app/src/App.js
+++ pc-app/src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
 import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import Testimonials from './components/Testimonials';
 import resumeData from './assets/resumeData.json'
 
 function App() {
