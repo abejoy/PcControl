eb7e901fde5159bc83a7bb368c63be37a8f4664e
Abraham Joys <abrahamjoys@Abrahams-MacBook-Pro.local>
2022-06-30 02:30:42 +0100

    adding . on sh file

--- run_me_on_pi.sh
+++ run_me_on_pi.sh
@@ -1,6 +1,6 @@
 cd pc-app
 docker image rm -f pcapp:prod
-docker build -t pcapp:prod
+docker build -t pcapp:prod .
 xterm -e "docker run -it --rm -p 80:80 pcapp:prod"
 # xterm -e "sudo serve -s build -l 80"
 # cd ../pc-backend
