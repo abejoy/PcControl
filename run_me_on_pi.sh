e030cbc8da308d149f43ff710a2693a3a0271d24
Abraham Joys <abrahamjoys@Abrahams-MacBook-Pro.local>
2022-06-30 03:09:08 +0100

    updating sh file

--- run_me_on_pi.sh
+++ run_me_on_pi.sh
@@ -2,7 +2,7 @@ cd pc-app
 docker rm -f pcapp
 docker image rm -f pcapp:prod
 docker build -t pcapp:prod .
-screen -d -m docker run -it --rm -p 80:80 pcapp:prod --name pcapp
+screen -d -m docker run -it --rm --name pcapp -p 80:80 pcapp:prod
 # xterm -e "sudo serve -s build -l 80"
 # cd ../pc-backend
 # xterm -e "sudo python3 run_on_pi.py"
