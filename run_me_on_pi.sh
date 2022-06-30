c683348f2ed0a51862fa83afd2920346e0c6c0a8
Abraham Joys <abrahamjoys@Abrahams-MacBook-Pro.local>
2022-06-30 03:23:11 +0100

    adding python to sh

--- run_me_on_pi.sh
+++ run_me_on_pi.sh
@@ -3,6 +3,6 @@ docker rm -f pcapp
 docker image rm -f pcapp:prod
 docker build -t pcapp:prod .
 screen -d -m docker run -it --rm --name pcapp -p 80:80 pcapp:prod
-# xterm -e "sudo serve -s build -l 80"
-# cd ../pc-backend
-# xterm -e "sudo python3 run_on_pi.py"
+# sudo serve -s build -l 80
+cd ../pc-backend
+screen -d -m sudo python3 run_on_pi.py
