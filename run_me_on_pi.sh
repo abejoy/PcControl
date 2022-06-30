13571867f3373259ff813f0a5f3952402a072776
Abraham Joys <abrahamjoys@Abrahams-MacBook-Pro.local>
2022-06-30 03:04:42 +0100

    ammended sh command

--- run_me_on_pi.sh
+++ run_me_on_pi.sh
@@ -1,7 +1,8 @@
 cd pc-app
+docker rm -f pcapp
 docker image rm -f pcapp:prod
 docker build -t pcapp:prod .
-docker run -it --rm -p 80:80 pcapp:prod
+screen -d -m docker run -it --rm -p 80:80 pcapp:prod --name pcapp
 # xterm -e "sudo serve -s build -l 80"
 # cd ../pc-backend
 # xterm -e "sudo python3 run_on_pi.py"
