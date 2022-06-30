0d0c38576aca438088a18d94f65f13db65303b06
Abraham Joys <abrahamjoys@Abrahams-MacBook-Pro.local>
2022-06-30 03:26:03 +0100

    fix spelling

--- run_me_on_pi.sh
+++ run_me_on_pi.sh
@@ -4,5 +4,5 @@ docker image rm -f pcapp:prod
 docker build -t pcapp:prod .
 screen -d -m docker run -it --rm --name pcapp -p 80:80 pcapp:prod
 # sudo serve -s build -l 80
-cd ../pc-backend
+cd ../pi-backend
 screen -d -m sudo python3 run_on_pi.py
