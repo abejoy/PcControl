4fb6eb9a81f50e8534c05d0f4a18678e31c5f13f
Abraham Joys <abrahamjoys@Abrahams-MacBook-Pro.local>
2022-07-29 20:58:04 +0100

    updated bash script to deploy docker aswell

--- backend/run_me_on_mac.sh
+++ backend/run_me_on_mac.sh
@@ -1,12 +1,19 @@
+prev_version=0.0.4
+version=0.0.5
+
 cd ../pc-app
 npm i
-rm -R ../backend/src/main/resources/static/*
+sudo rm -R ../backend/src/main/resources/static/*
 npm run build
-cp -R build/ ../backend/src/main/resources/static/
+sudo cp -R build/ ../backend/src/main/resources/static/
 
 cd ../backend
-docker image rm -f abrahamjoys98/lkcyl:0.0.3
+docker image rm -f abrahamjoys98/lkcyl:$prev_version
 sudo mvn clean package
 docker build -t pibackend:prod .
-docker tag pibackend:prod abrahamjoys98/lkcyl:0.0.4
-docker push abrahamjoys98/lkcyl:0.0.4
+docker tag pibackend:prod abrahamjoys98/lkcyl:$version
+docker push abrahamjoys98/lkcyl:$version
+
+
+bash -c "ssh root@www.lkcyl.com 'cd /; docker rm -f pibackend; docker image rm abrahamjoys98/lkcyl:$prev_version; docker run -d --name pibackend -v $(pwd):/home/docker/data -p 80:80 abrahamjoys98/lkcyl:$version'"
+
