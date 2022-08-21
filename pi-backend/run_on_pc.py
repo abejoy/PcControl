241b0afb86840d43f8fd9bc1f1ad63a1eb9f52f1
Abraham Joys <abrahamjoys@Abrahams-MacBook-Pro.local>
2022-08-21 19:10:37 +0100

    trying sleep commands

--- pi-backend/run_on_pc.py
+++ pi-backend/run_on_pc.py
@@ -6,35 +6,11 @@ import subprocess
 from flask import Flask, json
 
 api = Flask(__name__)
-my_song = None
-
-@api.route('/playParot', methods=['GET'])
-def playParot():
-    global my_song
-    my_song = vlc.MediaPlayer("Parot.mp3")
-    my_song.play()
-    # os.startfile('Parot.mp3')
-    return "playing"
-
-
-@api.route('/stop-parot', methods=['GET'])
-def stopParot():
-    global my_song
-    if(my_song != None):
-        my_song.stop()
-    return 'stoped'
 
 
 @api.route('/sleep', methods=['GET'])
 def sleepPc():
-    def do_work():
-        stopParot()
-        subprocess.call(['Sleep.bat'])
-        time.sleep(1)
-
-    thread = threading.Thread(target=do_work)
-    thread.start()
-
+    os.system("$echo sumasuja | sudo -S systemctl suspend")
     return 'sleeping'
 
 
